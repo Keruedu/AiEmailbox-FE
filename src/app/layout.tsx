7b4914549346ee3ea95353ac20f93d5b6a5381fa
Keru1712 <133781338+Keru1712@users.noreply.github.com>
2026-01-21 18:26:35 +0700

    refactor: change domain icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -63,6 +63,7 @@ export default function RootLayout({
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="theme-color" content="#667eea" />
         
+        <link rel="icon" type="image/svg+xml" href="/icons/icon.svg" />
         <link rel="icon" type="image/png" sizes="32x32" href="/icons/icon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/icons/icon-16x16.png" />
         <link rel="apple-touch-icon" href="/icons/icon-152x152.png" />
